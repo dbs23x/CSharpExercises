768f83b4284e9929e8a0cfcfede1ef68f82f5c81
dbs23x <dbs23x@gmail.com>
2023-01-06 23:38:27 -0300

    Add project files.

---
+++ CSharpExercises/Basics/01-AddTwoNumbers.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpExercises.Basics
+{
+    //Add two numbers
+    //Given three numbers, write a method that adds two first ones and multiplies them by a third one.
+    //Expected input and output
+    //AddAndMultiply(2, 4, 5) → 30
+
+    public class AddTwoNumbers
+    {
+        public double AddAndMultiply(double a, double b, double c)
+        {
+            return (a + b) * c;
+        }
+
+        public void Main()
+        {
+            Console.WriteLine(AddAndMultiply(3, 6, 35));        // 315
+            Console.WriteLine(AddAndMultiply(-12, 5, 17));      // -119
+            Console.WriteLine(AddAndMultiply(-40, 50, 60));     // 600
+            Console.WriteLine(AddAndMultiply(1.7, 9.9, 0.01));  // 0.116
+        }
+    }
+}
---
+++ CSharpExercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSharpExercises.Basics;
+
+Console.WriteLine(@"Menu:
+1 - 01-AddTwoNumbers.cs
+");
+
+Console.WriteLine("Select: ");
+var menu = Convert.ToInt32(Console.ReadLine());
+
+if (menu == 1)
+{
+    Console.Clear();
+    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+    addTwoNumbers.Main();
+}
