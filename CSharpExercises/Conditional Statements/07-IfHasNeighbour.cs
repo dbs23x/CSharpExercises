178d993a12d57fd579afa3fac6d5f9394cc1891f
dbs23x <dbs23x@gmail.com>
2023-01-20 23:57:14 -0300

    Add the C# solution

---
+++ CSharpExercises/Conditional Statements/07-IfHasNeighbour.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpExercises.Conditional_Statements
+{
+
+    //If has neighbour
+    //Given three letter long string, write a method that checks if at least one neighbour of middle letter is its neighbour in the alphabet.
+    //Expected input and output
+    //IsLonelyIsland("XYZ") → True
+    //IsLonelyIsland("GWK") → False
+
+    internal class _07_IfHasNeighbour
+    {
+        public static bool IfHasNeighbour(string word)
+        {
+            return word[0] == word[1] - 1 || word[0] == word[1] + 1 || word[2] == word[1] - 1 || word[2] == word[1] + 1;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(IfHasNeighbour("DCA")); // True
+            Console.WriteLine(IfHasNeighbour("PRT")); // False
+        }
+    }
+}
